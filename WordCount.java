5b001aa8c579535114e281e326a3b2647c160193
coltmarshmallow <josh.lees77@gmail.com>
2024-04-20 14:35:44 +0100

    Adding first pass github action with some structures

--- WordCount.java
+++ WordCount.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.TreeMap;
 
 public class WordCount {
